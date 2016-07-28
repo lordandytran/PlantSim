909cfdd43c3b80da3d74e5c23f5308af78404b33
Andy <thematulaaklives@gmail.com>
2016-07-27 18:06:21 -0700

    added example database connection and animation

--- README.txt
+++ README.txt
@@ -7,7 +7,7 @@ Host: http://students.washington.edu/lordandy/
 MySQL: vergil.u.washington.edu:252525/445ProjDB
 
 For Developers
-Username: user1
+Username: user
 Password: 445DataProj
 
 Here is the page with the sexy animation: http://codepen.io/maxakohler/pen/ltFyG
--- web/index.php
+++ web/index.php
@@ -1,6 +1,37 @@
+<?php
+	//Connect to MySQL
+	$link = mysql_connect('vergil.u.washington.edu:252525', 'user', '445DataProj');
+	if (!$link) {
+    		die('Not connected : ' . mysql_error());
+	}
+
+	// Connect to database
+	$db_selected = mysql_select_db('445ProjDB', $link);
+	if (!$db_selected) {
+    		die ('Can\'t connect to database : ' . mysql_error());
+	}	
+?>
+
 <html>
-    <body>
-    :) <br>
-    -Andy
-    </body>    
+<head>
+	<title>Plant Simulator</title>
+	<link rel="stylesheet" type="text/css" href="https://students.washington.edu/lordandy/css/style.css">
+</head>
+	<body>
+		<div class = 'fwrapper'>
+		<!-- Animation from http://codepen.io/maxakohler/pen/ltFyG -->
+		<div class='flower flower-0'><span></span></div>
+		<div class='flower flower-1'><span></span></div>
+		<div class='flower flower-2'><span></span></div>
+		<div class='flower flower-3'><span></span></div>
+		<div class='flower flower-4'><span></span></div>
+		<div class='flower flower-5'><span></span></div>
+		<div class='flower flower-6'><span></span></div>
+		</div>
+	</body>    
 </html>
+
+<?php
+	//Close Connection
+	mysql_close($link);
+?>
