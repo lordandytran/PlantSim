c10732f3c1ee35dc1f5523e0e364211723cde374
mike3651 <mike365@uw.edu>
2016-08-01 14:22:30 -0700

    Creating the js file

---
+++ web/webPage.js
@@ -0,0 +1,5 @@
+// This is to wait for all of the HTML elements to load before trying to manipulate them
+$(document).ready(function() {
+  // This will allow each div to fold under a header like an accordian
+  $('#Plant-segments').accordion();
+});
