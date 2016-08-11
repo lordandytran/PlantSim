b8928589fba5594f747fa50abd95e9f786c84d62
Alex Terikov <teraliv@gmail.com>
2016-08-11 12:57:57 -0700

    Update plants_table.sql

--- plants_table.sql
+++ plants_table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Aug 03, 2016 at 06:16 AM
+-- Generation Time: Aug 11, 2016 at 09:56 PM
 -- Server version: 5.5.42
 -- PHP Version: 7.0.0
 
@@ -22,18 +22,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `plants` (
   `plant_id` int(11) NOT NULL,
-  `plant_name` varchar(45) NOT NULL
+  `plant_name` varchar(45) NOT NULL,
+  `genus` varchar(45) DEFAULT NULL,
+  `species` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `plants`
 --
 
-INSERT INTO `plants` (`plant_id`, `plant_name`) VALUES
-(4, 'calendula'),
-(3, 'chamomile'),
-(1, 'rose'),
-(2, 'tulip');
+INSERT INTO `plants` (`plant_id`, `plant_name`, `genus`, `species`) VALUES
+(1, 'Rose', 'Rosa', 'Damascena'),
+(2, 'Tulip', 'Tulipa', 'Tulipa gesneriana'),
+(3, 'Chamomile', 'Matricaria', 'Matricaria cammomilla'),
+(4, 'Calendula', 'Calendula', 'Calendula arvensis');
 
 --
 -- Indexes for dumped tables
